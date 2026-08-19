3a788a72a9d89aa64746b5be3c562deea3917334
agbonzeprincessosasogie-alt <princessagbonze5@gmail.com>
2026-08-19 12:32:46 +0100

    refactor API and add automated tests

--- server.js
+++ server.js
@@ -1,32 +1,15 @@
-const express = require("express");
 const mongoose = require("mongoose");
-require("dotenv").config();
-
-const authRoutes = require("./routes/authRoutes");
-const authMiddleware = require("./middlewares/authMiddleware");
-const postRoutes = require("./routes/postRoutes");
-const app = express();
-
-app.use(express.json());
-
-app.use("/api/auth", authRoutes);
-app.use("/api/posts", postRoutes);
+const app = require("./app");
 
 mongoose
     .connect(process.env.MONGO_URI)
     .then(() => {
         console.log("Database Connected");
+
+        app.listen(process.env.PORT, () => {
+            console.log(`Server running on port ${process.env.PORT}`);
+        });
     })
     .catch((err) => {
-        console.error(err);
+        console.error("Database connection failed:", err);
     });
-app.get("/api/protected", authMiddleware, (req, res) => {
-    res.json({
-        message: "You have access to this protected route!",
-        user: req.user
-    });
-});
-
-app.listen(process.env.PORT, () => {
-    console.log(`Server running on port {process.env.PORT}`);
-});
